c48cc6fe324783dd7f733c17295d8f2c55178ed6
Vaktech <vaktech4@gmail.com>
2023-12-17 20:55:04 +0100

    custom fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 import DayListItem from './src/component/core/DayListItem';
+import { useFonts, Inter_900Black, Inter_700Bold } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen'; 
+import { useEffect } from 'react';
+import { AmaticSC_400Regular, AmaticSC_700Bold } from '@expo-google-fonts/amatic-sc'
+
+SplashScreen.preventAutoHideAsync
 
 const days = [...Array(24)].map ((val, index)=> index + 1);
 
 export default function App() {
+  const [fontsLoaded , fontError] = useFonts({
+    Inter: Inter_700Bold,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold,
+  })
+
+
+  useEffect (()=>{
+
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }; 
+  }, [fontsLoaded, fontError])
+
+  
+  if (!fontsLoaded && !fontError) {
+    SplashScreen.hideAsync();
+  }; 
+
+  
   return (
 
     <View style={styles.container}>
@@ -20,7 +46,7 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   );
-}
+    }
 
 const styles = StyleSheet.create({
   container: {
