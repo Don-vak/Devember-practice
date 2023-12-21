534174f142431470d3e57e9069a7357c63ceeaec
Vaktech <vaktech4@gmail.com>
2023-12-21 13:53:58 +0100

    custom path to tsconfig

--- src/app/(days)/index.tsx
+++ src/app/(days)/index.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
-import DayListItem from '../../component/core/DayListItem';
+import DayListItem from '@component/core/DayListItem';
 
 
 
